1520e7535250e23ace2a65cc68d03e6982a89bbc
Maxime Imbeau <maxime.imbeau.1@ulaval.ca>
2022-02-21 15:06:44 -0500

    test: add unit test for sellerFactory

---
+++ src/test/java/ulaval/glo2003/application/seller/SellerFactoryTest.java
@@ -0,0 +1,76 @@
+package ulaval.glo2003.application.seller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ulaval.glo2003.controllers.exceptions.InvalidParameterException;
+import ulaval.glo2003.controllers.exceptions.MissingParameterException;
+import ulaval.glo2003.domain.Seller;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class SellerFactoryTest {
+    private static final String NAME = "Carey Price";
+    private static final String BIO = "Hockey";
+    private static final LocalDate VALID_BIRTH_DATE = LocalDate.of(1987, 8, 16);
+    private static final LocalDate INVALID_BIRTH_DATE = LocalDate.of(2004, 8, 16);
+
+    private SellerFactory sellerFactory;
+
+    @BeforeEach
+    public void setUp() {
+        sellerFactory = new SellerFactory();
+    }
+
+    @Test
+    public void givenASeller_whenNameIsEmpty_thenThrowsInvalidParameterException() {
+        assertThrows(InvalidParameterException.class, () ->
+                sellerFactory.createSeller("", BIO, VALID_BIRTH_DATE));
+    }
+
+    @Test
+    public void givenASeller_whenBioIsEmpty_thenThrowsInvalidParameterException() {
+        assertThrows(InvalidParameterException.class, () ->
+                sellerFactory.createSeller(NAME, "", VALID_BIRTH_DATE));
+    }
+
+    @Test
+    public void givenASeller_whenAgeIsUnder18YearsOld_thenThrowsInvalidParameterException() {
+        assertThrows(InvalidParameterException.class, () ->
+                sellerFactory.createSeller(NAME, BIO, INVALID_BIRTH_DATE));
+    }
+
+    @Test
+    public void givenASeller_whenNameIsNotInTheRequest_thenThrowsMissingParameterException() {
+        assertThrows(MissingParameterException.class, () ->
+                sellerFactory.createSeller(null, BIO, INVALID_BIRTH_DATE));
+    }
+
+    @Test
+    public void givenASeller_whenBioIsNotInTheRequest_thenThrowsMissingParameterException() {
+        assertThrows(MissingParameterException.class, () ->
+                sellerFactory.createSeller(NAME, null, INVALID_BIRTH_DATE));
+    }
+
+    @Test
+    public void givenASeller_whenBirthDateIsNotInTheRequest_thenThrowsMissingParameterException() {
+        assertThrows(MissingParameterException.class, () ->
+                sellerFactory.createSeller(NAME, BIO, null));
+    }
+
+    @Test
+    public void whenCreating_thenSellerHasAnIdGenerated() {
+        Seller seller = sellerFactory.createSeller(NAME, BIO, VALID_BIRTH_DATE);
+
+        assertNotNull(seller.getId());
+    }
+
+    @Test
+    public void whenCreating_thenSellerHasATimeOfCreation() {
+        Seller seller = sellerFactory.createSeller(NAME, BIO, VALID_BIRTH_DATE);
+
+        assertNotNull(seller.getCreatedAt());
+    }
+}
