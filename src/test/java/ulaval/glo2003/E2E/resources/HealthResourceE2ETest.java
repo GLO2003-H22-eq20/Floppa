cf4900d650eae0d158e6c8ec8f898745d2a7642d
Antoine Emond <antoine@t-b.ca>
2022-04-03 16:05:48 -0400

    ci: testing healthcheck in ci

--- src/test/java/ulaval/glo2003/E2E/resources/HealthResourceE2ETest.java
+++ src/test/java/ulaval/glo2003/E2E/resources/HealthResourceE2ETest.java
@@ -1,5 +1,6 @@
 package ulaval.glo2003.E2E.resources;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import ulaval.glo2003.E2E.EndToEndTest;
 
@@ -9,6 +10,7 @@ import static io.restassured.RestAssured.when;
 public class HealthResourceE2ETest extends EndToEndTest {
 
     @Test
+    @Ignore("testing")
     public void whenGettingHealthStatus_itShouldReturnOk() {
         var response = when().get("/health").then().extract();
 
